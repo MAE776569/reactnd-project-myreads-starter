4c615d41e56ebbbf2b10996be9b03fb4208cdda0
MAE776569 <mohamed.ahmed.ezzat776569@gmail.com>
2019-09-24 13:03:13 +0200

    Update change category to handle searched books

--- src/App.js
+++ src/App.js
@@ -7,21 +7,13 @@ import SearchBooks from "./SearchBooks";
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
     books: []
   };
 
   componentDidMount() {
     //Get all books and update state
     BooksAPI.getAll().then(books => {
-      this.setState({
-        books
-      });
+      this.setState({ books });
     });
   }
 
@@ -30,20 +22,32 @@ class BooksApp extends React.Component {
     const bookIndex = this.state.books.findIndex(b => b.id === book.id);
     //Update the book in the database
     BooksAPI.update(book, shelf);
-    
-    //Change the state
-    this.setState(prevState => {
-      const books = prevState.books;
-      books[bookIndex].shelf = shelf;
-      return { books };
-    });
+
+    //If the book found change the shelf
+    if (bookIndex !== -1) {
+      this.setState(prevState => {
+        const books = prevState.books;
+        books[bookIndex].shelf = shelf;
+        return { books };
+      });
+    }
+    //else add it to the books
+    else {
+      book.shelf = shelf;
+      this.setState(prevState => {
+        const books = prevState.books;
+        books.push(book);
+        return { books };
+      });
+    }
   };
 
   render() {
     return (
       <div className="app">
         <Route
-          exact path="/"
+          exact
+          path="/"
           render={() => (
             <BookCategories
               books={this.state.books}
@@ -52,7 +56,18 @@ class BooksApp extends React.Component {
           )}
         />
 
-        <Route path="/search" component={SearchBooks} />
+        <Route
+          path="/search"
+          render={({ history }) => (
+            <SearchBooks
+              libraryBooks={this.state.books}
+              onChangeCategory={(shelf, book) => {
+                this.changeCategory(shelf, book);
+                history.push("/");
+              }}
+            />
+          )}
+        />
       </div>
     );
   }
